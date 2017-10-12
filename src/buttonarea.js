d6623bd74e450df25a66d287a568c95e2b2ac425
tomsky <tomsky@posteo.de>
2017-10-12 17:35:50 +0200

    nichts erreicht

--- src/buttonarea.js
+++ src/buttonarea.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 
-function myfunction(event){
-  console.log('hello there')
-}
-
 class ButtonArea extends Component {
+  constructor(props){
+    super(props);
+  }
+  myfunction(){
+    document.getElementById("App").style.backgroundColor = "yellow";
+  }
 
   render(){
     return(
       <div className="flex-item flex-item-bottom">
         <p>text</p>
-        <Button onClick={myfunction}>
+        <Button onClick={this.myfunction.bind()}>
           Default
         </Button>
       </div>
