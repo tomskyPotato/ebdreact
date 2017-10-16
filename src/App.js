1fa5f2b5085bef39ea01ea6ca56e92ee6a403eb8
tomsky <tomsky@posteo.de>
2017-10-16 16:44:11 +0200

    router ein anfang ist gemacht

--- src/App.js
+++ src/App.js
@@ -5,23 +5,13 @@ import Body from './body.js';
 import ButtonArea from './buttonarea.js';
 
 class App extends Component {
-constructor(props){
-  super(props);
-  this.state = {temperature: 0};
-}
-
-handleClick = () => {
-  let n = this.state.temperature;
-  n += 1;
-  this.setState({temperature: n});
-}
 
   render() {
     return (
       <div className="App">
         <Navi />
         <Body />
-        <ButtonArea name="Thomas" handleClick={this.handleClick} temperature={this.state.temperature}/>
+        <ButtonArea />
       </div>
     );
   }
--- src/body.js
+++ src/body.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, Jumbotron } from 'react-bootstrap';
 import './App.css';
+import { Router, Route, BrowserRouter } from 'react-router-dom'
 
 const divStyle = {
   textAlign: 'center',
@@ -10,14 +11,39 @@ class Body extends Component {
     render(){
       return(
         <div className="flex-item flex-item-body">
-          <Jumbotron style={divStyle}>
-            <h1>Willkommen bei EBD</h1>
-            <p>Willkommen beim Entwicklungsbeobachtungs und Dokumentationstool!</p>
-            <p><Button bsStyle="primary">Infos</Button></p>
-          </Jumbotron>
+          <BrowserRouter>
+            <div>
+              <Route path='/' component={welcome} />
+              <Route path='/' component={mathilda} />
+            </div>
+          </BrowserRouter>
         </div>
       );
     }
   }
+
+  class welcome extends Component{
+    render(){
+      return(
+        <Jumbotron style={divStyle}>
+          <h1>Willkommen bei EBD</h1>
+          <p>Willkommen beim Entwicklungsbeobachtungs und Dokumentationstool!</p>
+          <p><Button bsStyle="primary">Infos</Button></p>
+        </Jumbotron>
+      );
+    }
+  }
+  
+    class mathilda extends Component{
+      render(){
+        return(
+          <Jumbotron style={divStyle}>
+            <h1>Willkommen bei Mathilda und Lilly</h1>
+            <p>Willkommen beim Entwicklungsbeobachtungs und Dokumentationstool!</p>
+            <p><Button bsStyle="primary">Infos</Button></p>
+          </Jumbotron>
+        );
+      }
+    }
 
   export default Body;
