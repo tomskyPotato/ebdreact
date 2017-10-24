154402d3b349e68c46a2c44a6992981c458f26da
tomsky <tomsky@posteo.de>
2017-10-24 15:10:41 +0200

    router von body.js nach app.js verschoben. body.js gelöscht

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import Body from './body.js';
+import loginClass from './login.js';
+import Navi from './navbar.js';
+import ButtonArea from './buttonarea.js';
+import { Button, Jumbotron } from 'react-bootstrap';
+
+const divStyle = {
+  textAlign: 'center',
+}
 
 class App extends Component {
 
@@ -9,11 +16,33 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className="App">
-          <Body />
+          <Route path='/' exact component={welcome} />
+          <Route path='/mathildaPath' component={Navi} />
+          <Route path='/mathildaPath' component={mathilda} />
+          <Route path='/mathildaPath' component={ButtonArea} />
+          <Route path='/loginPath' component={loginClass} />
         </div>
       </BrowserRouter>
     );
   }
 }
+
+const welcome =() => 
+  <Jumbotron style={divStyle}>
+    <h1>Willkommen bei EBD</h1>
+    <p>Willkommen beim Entwicklungsbeobachtungs und Dokumentationstool!</p>
+    <br/>
+    <p><Button bsStyle="primary" href="./loginPath">Anmelden</Button></p>
+    <p><Button bsStyle="primary" href="./welcomePath">Registrieren</Button></p>
+  </Jumbotron>
+
+const mathilda =() =>
+<div>
+  <Jumbotron style={divStyle}>
+    <h1>Willkommen bei Mathilda und Lilly</h1>
+    <p>Willkommen beim Entwicklungsbeobachtungs und Dokumentationstool!</p>
+    <p><Button bsStyle="primary" href="./">Home</Button></p>
+  </Jumbotron>
+</div>
 
 export default App;
