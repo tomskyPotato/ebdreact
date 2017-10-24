8e7958ecc8dc96c24c2b4f33984cb7d6cb3e03d6
tomsky <tomsky@posteo.de>
2017-10-24 15:07:27 +0200

    router routing wird komplett nach app verschoben

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import Navi from './navbar.js';
 import Body from './body.js';
-import ButtonArea from './buttonarea.js';
 
 class App extends Component {
 
@@ -11,9 +9,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className="App">
-          <Route path='/mathilda' exact component={Navi} />
           <Body />
-          <Route path='/mathilda' exact component={ButtonArea} />
         </div>
       </BrowserRouter>
     );
--- src/body.js
+++ src/body.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Button, Jumbotron } from 'react-bootstrap';
 import { Route } from 'react-router-dom'
+import loginClass from './login.js';
+import Navi from './navbar.js';
+import ButtonArea from './buttonarea.js';
 
 const divStyle = {
   textAlign: 'center',
@@ -11,7 +14,10 @@ class Body extends Component {
       return(
           <div>
             <Route path='/' exact component={welcome} />
-            <Route path='/mathilda' component={mathilda} />
+            <Route path='/mathildaPath' component={Navi} />
+            <Route path='/mathildaPath' component={mathilda} />
+            <Route path='/mathildaPath' component={ButtonArea} />
+            <Route path='/loginPath' component={loginClass} />
           </div>
       );
     }
@@ -22,15 +28,17 @@ const welcome =() =>
     <h1>Willkommen bei EBD</h1>
     <p>Willkommen beim Entwicklungsbeobachtungs und Dokumentationstool!</p>
     <br/>
-    <p><Button bsStyle="primary" href="./mathilda">Anmelden</Button></p>
-    <p><Button bsStyle="primary" href="./mathilda">Registrieren</Button></p>
+    <p><Button bsStyle="primary" href="./loginPath">Anmelden</Button></p>
+    <p><Button bsStyle="primary" href="./welcomePath">Registrieren</Button></p>
   </Jumbotron>
 
 const mathilda =() =>
+<div>
   <Jumbotron style={divStyle}>
     <h1>Willkommen bei Mathilda und Lilly</h1>
     <p>Willkommen beim Entwicklungsbeobachtungs und Dokumentationstool!</p>
     <p><Button bsStyle="primary" href="./">Home</Button></p>
   </Jumbotron>
+</div>
 
-  export default Body;
+export default Body;
